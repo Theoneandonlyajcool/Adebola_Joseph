b04d254d8686a76d70e759455a00eff1b5f499e7
Theoneandonlyajcool <ac1106508@gmail.com>
2026-01-15 15:42:41 +0100

    Made some UI Changes

--- src/components/ProdModal.tsx
+++ src/components/ProdModal.tsx
@@ -2,16 +2,16 @@ import { Button } from "@/components/ui/button";
 interface ProdModalProps {
   closeModal: (value: boolean) => void;
   link?: string;
+  text: string;
 }
 
-const ProdModal = ({ closeModal, link }: ProdModalProps) => {
+const ProdModal = ({ closeModal, link, text }: ProdModalProps) => {
   return (
     <div className="bg-black/60 shadow-2xl shadow-gray-900 backdrop-blur-xs fixed w-full h-screen top-0 z-1000 flex justify-center items-center text-white p-4">
       <div className="bg-white flex justify-center items-center text-gray-900 w-[98%] h-[60%] md:w-[50%] lg:max-w-[40%] md:max-w-7xl md:h-[70%] lg:h-[60%] rounded-3xl">
         <div className="w-full max-w-[90%] h-full text-center flex justify-center items-center flex-col">
           <h1 className="text-xl md:text-2xl  2xl:text-3xl font-bold">
-            This project is currently under production <br /> as changes haven't
-            been deployed yet.
+            {text}
           </h1>
 
           <p className="mt-4">Do you still wish to continue ?</p>
